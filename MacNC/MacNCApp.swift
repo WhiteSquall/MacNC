16259efcc2e7baa091788fc3f01c61588e1a7485
whitesquall@gmail.com <k1kmh@hotmail.com>
2023-09-23 23:06:27 +0900

    Initial Commit

---
+++ MacNC/MacNCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacNCApp.swift
+//  MacNC
+//
+//  Created by Myeonghui Kim on 2023/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MacNCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
